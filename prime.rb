24444e9bb702026af64e48f38b6247681a1b478c
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 04:07:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,1 @@
-def prime?(number)
-  range = (2..(number - 1)).to_a
-  check = true
-  if number < 2
-    check = false
-  else
-    range.each do |x|
-      if number % x == 0
-        check = false
-        break
-      end
-    end
-  end
-   puts check
-end
+
